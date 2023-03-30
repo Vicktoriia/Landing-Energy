774813fe2232bbf4f587ef6511e90c4bbc8be86a
Viktoriia Aleksieieva <105809872+Vicktoriia@users.noreply.github.com>
2023-03-30 12:57:43 +0300

    orange style

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,9 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            <i class="fa fa-lightbulb-o">
+              <span>GRANDEX</span>
+            </i>
           </a>{" "}
         </div>
 
@@ -28,11 +30,6 @@ export const Navigation = (props) => {
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#features" className="page-scroll">
-                Features
-              </a>
-            </li>
-            <li>
               <a href="#about" className="page-scroll">
                 About
               </a>
@@ -41,20 +38,23 @@ export const Navigation = (props) => {
               <a href="#services" className="page-scroll">
                 Services
               </a>
+              <ul>
+                {" "}
+                <li>
+                  <a href="#testimonials" className="page-scroll">
+                    Testimonials
+                  </a>
+                </li>
+                <li>
+                  <a href="#team" className="page-scroll">
+                    Team
+                  </a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href="#portfolio" className="page-scroll">
                 Gallery
-              </a>
-            </li>
-            <li>
-              <a href="#testimonials" className="page-scroll">
-                Testimonials
-              </a>
-            </li>
-            <li>
-              <a href="#team" className="page-scroll">
-                Team
               </a>
             </li>
             <li>
